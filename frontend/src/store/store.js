c6ae60c74798a471b881d5a97c69bfa0a60150fb
linarifux <linarifux@gmail.com>
2026-05-03 14:04:03 +0600

    module updated

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './slices/cartSlice';
-import authReducer from './slices/authSlice';
-import { apiSlice } from './apiSlice';
+import cartReducer from './slices/cartSlice.js';
+import authReducer from './slices/authSlice.js';
+import { apiSlice } from './apiSlice.js';
 
 export const store = configureStore({
   reducer: {
