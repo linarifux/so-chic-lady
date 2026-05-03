751d653bba93d9e4a171d8a0c54e6c3ed0134d32
linarifux <linarifux@gmail.com>
2026-05-03 14:08:42 +0600

    apiSlice file created again with correct name

---
+++ frontend/src/store/apiSlice.js
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+// Define our base API URL (pointing to our Node/Express backend)
+const baseQuery = fetchBaseQuery({ baseUrl });
+
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ['Product', 'User'], // Used for automatic caching and re-fetching
+  endpoints: (builder) => ({}), // We will inject endpoints in separate files to keep code clean
+});
